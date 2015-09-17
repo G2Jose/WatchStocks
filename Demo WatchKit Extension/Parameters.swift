6a3b568f390ab388e0ae0093efb2070113b740b6
willedflipper66 <george.jose.a@gmail.com>
2015-09-17 09:45:23 -0400

    Update Parameters.swift

--- Demo WatchKit Extension/Parameters.swift
+++ Demo WatchKit Extension/Parameters.swift
@@ -17,37 +17,37 @@ class Parameters{
     static var myStocks: [String: [String:Double]] = [
         "AAPL+NASDAQ": [
             "buyPrice": 131.73,
-            "quantity": 17,
+            "quantity": 1,
             "commission":6.95
         ],
         "WSP+Toronto": [
             "buyPrice": 37.89,
-            "quantity": 59,
+            "quantity": 1,
             "commission":6.95
         ],
         "CNR+TSX": [
             "buyPrice": 74.65,
-            "quantity": 17,
+            "quantity": 1,
             "commission":6.95
         ],
         "Algonquin+Power": [
             "buyPrice": 9.31,
-            "quantity": 80,
+            "quantity": 1,
             "commission":6.95
         ],
         "BTB.UN": [
             "buyPrice": 4.52,
-            "quantity": 167,
+            "quantity": 1,
             "commission":6.95
         ],
         "altagas+tsx": [
             "buyPrice": 35.57,
-            "quantity": 43,
+            "quantity": 1,
             "commission":6.95
         ],
         "GIB.A": [
             "buyPrice": 47.93,
-            "quantity": 24,
+            "quantity": 1,
             "commission":6.95
         ]
     ]
